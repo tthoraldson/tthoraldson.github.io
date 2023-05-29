3abe61b5e0eba38d0405020f88671a1d162ded4e
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2023-05-29 09:27:45 -0500

    starts to modify flake

--- flake.nix
+++ flake.nix
@@ -20,8 +20,7 @@
             layers = [ ./. ];
             layersString = [ "/" ];
             # port = 8080;
-            baseUrl = "./."; # Change to "/" (or remove it entirely) if using CNAME
-            # prettyUrls = true;
+            prettyUrls = true;
           };
         };
         devShells.default = pkgs.mkShell {
